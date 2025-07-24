f0158d50d4566f1a5cda0e54137f857525ae20e9
zhangjianshe <zhangjianshe@gmail.com>
2025-07-24 12:38:43 +0800

    Bump version to 0.0.23

--- main.go
+++ main.go
@@ -21,7 +21,7 @@ import (
 //go:embed static/*
 var staticFiles embed.FS
 
-var APP_VERSION = "0.0.22"
+var APP_VERSION = "0.0.23"
 
 // SirServer struct defines the server's metadata
 type SirServer struct {
